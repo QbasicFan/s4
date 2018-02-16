ce7e9993e64f1761a34e716434506141c8a7fdf3
QbasicFan <philSerme@gmail.com>
2018-02-16 00:45:05 -0500

    Create admin.py

---
+++ admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import front , section1 , section2
+
+admin.site.register(front)
+admin.site.register(section1)
+admin.site.register(section2)
+

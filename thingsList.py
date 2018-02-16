8e3bf8d5b277b126081dc84368b25c41060ef835
QbasicFan <philSerme@gmail.com>
2018-02-16 00:46:06 -0500

    Create thingsList.py

---
+++ thingsList.py
@@ -0,0 +1,48 @@
+
+'''
+List for colors, font size , and font type
+
+'''
+
+color = (
+    ("w3-red" , "Red"),
+    ("w3-pink" , "Pink"),
+    ("w3-purple" , "Purple"),
+    ("w3-deep-purple" , "Deep Purple"),
+    ("w3-indigo" , "Indigo"),
+    ("w3-blue" , "Blue"),
+    ("w3-light-blue" , "Light Blue"),
+    ("w3-cyan" , "Cyan"),
+    ("w3-aqua" , "Aqua"),
+    ("w3-teal" , "Teal"),
+    ("w3-green" , "Green"),
+    ("w3-light-green" , "Light Green"),
+    ("w3-lime" , "Lime"),
+    ("w3-sand" , "Sand"),
+    ("w3-khaki" , "Khaki"),
+    ("w3-yellow" , "Yellow"),
+    ("w3-amber" , "Amber"),
+    ("w3-orange" , "Orange"),
+    ("w3-deep-orange" , "Deep Orange"),
+    ("w3-blue-grey" , "Blue Grey"),
+    ("w3-brown" , "Brown"),
+    ("w3-light-grey" , "Light Grey"),
+    ("w3-grey" , "Grey"),
+    ("w3-dark-grey" , "Dark Grey"),
+    ("w3-pale-red" , "Pale Red"),
+    ("w3-pale-yellow" , "Pale Yellow"),
+    ("w3-pale-green" , "Pale Green"),
+    ("w3-pale-blue" , "Pale Blue"),
+)
+
+posi = (
+    ("w3-display-topleft","Top Left"),
+    ("w3-display-topmiddle","Top Middle"),
+    ("w3-display-topright","Top Right"),
+    ("w3-display-left","Left"),
+    ("w3-display-middle","Middle"),
+    ("w3-display-right","Right"),
+    ("w3-display-bottomleft","Bottom Left"),
+    ("w3-display-bottommiddle","Bottom Middle"),
+    ("w3-display-bottomright","Bottom Right"),
+)
